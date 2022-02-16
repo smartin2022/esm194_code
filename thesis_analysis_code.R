afe02ccedacedc4a533689e49db8a170bb13b7e3
Sarah Martin <sasmartin@ucdavis.edu>
2022-02-16 12:30:40 -0800

    started bringing in data

---
+++ thesis_analysis_code.R
@@ -0,0 +1,6 @@
+setwd("C:/Users/smart/Desktop/Honors Thesis/analysis")
+locations<- read.csv("CT lab data - combined location data.csv")
+camera_trap_nights<- read.csv("CT lab data - malfunction info.csv")
+main_data<- read.csv("Maromizaha Camera Trap Data - Sheet1.csv")
+
+#
